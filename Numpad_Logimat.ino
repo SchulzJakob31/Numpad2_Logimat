fc058d3c5b1be3ad5df68e305e1d1bba3ee916d2
Jakob Schulz <jakobschulz83@gmail.com>
2026-07-19 20:01:56 +0200

    Batterieanzeige verbessert (aufgeräumt)

--- Numpad_Logimat.ino
+++ Numpad_Logimat.ino
@@ -2,12 +2,14 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-uint8_t LCD_batterySymbol0[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B10001, B11111 };  //Symbol Batterie 0%
-uint8_t LCD_batterySymbol1[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B11111, B11111 };  //Symbol Batterie 20%
-uint8_t LCD_batterySymbol2[8] = { B01110, B11111, B10001, B10001, B10001, B11111, B11111, B11111 };  //Symbol Batterie 40%
-uint8_t LCD_batterySymbol3[8] = { B01110, B11111, B10001, B10001, B11111, B11111, B11111, B11111 };  //Symbol Batterie 60%
-uint8_t LCD_batterySymbol4[8] = { B01110, B11111, B10001, B11111, B11111, B11111, B11111, B11111 };  //Symbol Batterie 80%
-uint8_t LCD_batterySymbol5[8] = { B01110, B11111, B11111, B11111, B11111, B11111, B11111, B11111 };  //Symbol Batterie 100%
+uint8_t LCD_batterySymbol[6][8]{
+  { B01110, B11111, B10001, B10001, B10001, B10001, B10001, B11111 },  //Symbol Batterie 0%
+  { B01110, B11111, B10001, B10001, B10001, B10001, B11111, B11111 },  //Symbol Batterie 20%
+  { B01110, B11111, B10001, B10001, B10001, B11111, B11111, B11111 },  //Symbol Batterie 40%
+  { B01110, B11111, B10001, B10001, B11111, B11111, B11111, B11111 },  //Symbol Batterie 60%
+  { B01110, B11111, B10001, B11111, B11111, B11111, B11111, B11111 },  //Symbol Batterie 80%
+  { B01110, B11111, B11111, B11111, B11111, B11111, B11111, B11111 }   //Symbol Batterie 100%
+};
 
 const uint16_t LCD_batteryBlinkIntervall = 500;  //Blinkintervall der Balken der Batterie während des ladens
 uint32_t LCD_batteryBlinkLastMillis;             //Letzte millis beim ändern des Blinkzustands
@@ -26,28 +28,9 @@ void LCD_battery() {
     if (LCD_batteryBlinkState == 0) LCD_batteryChargePostBlink = LCD_batteryCharge;  // Bei 0 von Variable LCD_batteryBlinkState normalen Batteriestand (ohne Blink an) anzeigen
     else LCD_batteryChargePostBlink = LCD_batteryCharge + 1;                         // Bei 1 von Variable LCD_batteryBlinkState Batteriestand (mit Blink an) anzeigen
   }
-  switch (LCD_batteryChargePostBlink) {  //Symbol je nach Batteriestand neu zuweisen
-    case 0:
-      lcd.createChar(0, LCD_batterySymbol0);
-      break;
-    case 1:
-      lcd.createChar(0, LCD_batterySymbol1);
-      break;
-    case 2:
-      lcd.createChar(0, LCD_batterySymbol2);
-      break;
-    case 3:
-      lcd.createChar(0, LCD_batterySymbol3);
-      break;
-    case 4:
-      lcd.createChar(0, LCD_batterySymbol4);
-      break;
-    case 5:
-      lcd.createChar(0, LCD_batterySymbol5);
-      break;
-  }
-  lcd.setCursor(0, 0);  //Cursor an gewünschte stelle für das Batterysymbol setzen
-  lcd.write(0);         //Batteriesysmbol schreiben
+  lcd.createChar(0, LCD_batterySymbol[LCD_batteryChargePostBlink]);  //Symbol je nach Batteriestand neu zuweisen
+  lcd.setCursor(0, 0);                                               //Cursor an gewünschte stelle für das Batterysymbol setzen
+  lcd.write(0);                                                      //Batteriesysmbol schreiben
 }
 
 void setup() {

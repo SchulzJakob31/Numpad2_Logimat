f3b48fd56ec358a3aa0b23800dc8ea8aed1412bf
Jakob Schulz <jakobschulz83@gmail.com>
2026-07-19 18:25:40 +0200

    Batterieanzeige fertiggestellt

--- Numpad_Logimat.ino
+++ Numpad_Logimat.ino
@@ -2,75 +2,52 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-uint8_t Battery_0[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B10001, B11111 };
-uint8_t Battery_1[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B11111, B11111 };
-uint8_t Battery_2[8] = { B01110, B11111, B10001, B10001, B10001, B11111, B11111, B11111 };
-uint8_t Battery_3[8] = { B01110, B11111, B10001, B10001, B11111, B11111, B11111, B11111 };
-uint8_t Battery_4[8] = { B01110, B11111, B10001, B11111, B11111, B11111, B11111, B11111 };
-uint8_t Battery_5[8] = { B01110, B11111, B11111, B11111, B11111, B11111, B11111, B11111 };
+uint8_t LCD_batterySymbol0[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B10001, B11111 };  //Symbol Batterie 0%
+uint8_t LCD_batterySymbol1[8] = { B01110, B11111, B10001, B10001, B10001, B10001, B11111, B11111 };  //Symbol Batterie 20%
+uint8_t LCD_batterySymbol2[8] = { B01110, B11111, B10001, B10001, B10001, B11111, B11111, B11111 };  //Symbol Batterie 40%
+uint8_t LCD_batterySymbol3[8] = { B01110, B11111, B10001, B10001, B11111, B11111, B11111, B11111 };  //Symbol Batterie 60%
+uint8_t LCD_batterySymbol4[8] = { B01110, B11111, B10001, B11111, B11111, B11111, B11111, B11111 };  //Symbol Batterie 80%
+uint8_t LCD_batterySymbol5[8] = { B01110, B11111, B11111, B11111, B11111, B11111, B11111, B11111 };  //Symbol Batterie 100%
 
-bool blink = 0;
-uint32_t last = 0;
-const uint16_t intervall = 500;
+const uint16_t LCD_batteryBlinkIntervall = 500;  //Blinkintervall der Balken der Batterie während des ladens
+uint32_t LCD_batteryBlinkLastMillis;             //Letzte millis beim ändern des Blinkzustands
+bool LCD_batteryBlinkState;                      //Ob der blinkende Balken gerade angezeigt wird oder nicht
+uint8_t LCD_batteryChargePostBlink;              //Variable enthält Batteriestand der beim laden hoch und runter blinkt
+bool LCD_batteryIsCharging = 0;                  //Ob die Batterie gerade läd oder nicht
+uint8_t LCD_batteryCharge = 0;                   //Batteriestand der Batterie
 
-void LCD_battery(bool charging, uint8_t charge) {
-  if (charging == 0) {
-    switch (charge) {
-      case 0:
-        lcd.createChar(0, Battery_0);
-        break;
-      case 1:
-        lcd.createChar(0, Battery_1);
-        break;
-      case 2:
-        lcd.createChar(0, Battery_2);
-        break;
-      case 3:
-        lcd.createChar(0, Battery_3);
-        break;
-      case 4:
-        lcd.createChar(0, Battery_4);
-        break;
-      case 5:
-        lcd.createChar(0, Battery_5);
-        break;
+void LCD_battery() {
+  if (LCD_batteryIsCharging == 0) LCD_batteryChargePostBlink = LCD_batteryCharge;  //Wenn die Batterie nicht läd nicht blinken lassen
+  else {                                                                           //Wenn die Batterie läd blinken lassen
+    if (millis() - LCD_batteryBlinkLastMillis >= LCD_batteryBlinkIntervall) {      //Variable LCD_batteryBlinkState blinken lassen
+      LCD_batteryBlinkState = !LCD_batteryBlinkState;
+      LCD_batteryBlinkLastMillis = millis();
     }
-  } else if (charging == 1) {
-
-    if (millis() - last >= intervall) {
-      blink = !blink;
-      last = millis();
-    }
-
-    switch (charge) {
-      case 0:
-        if (blink == 0) lcd.createChar(0, Battery_0);
-        else lcd.createChar(0, Battery_1);
-        break;
-      case 1:
-        if (blink == 0) lcd.createChar(0, Battery_1);
-        else lcd.createChar(0, Battery_2);
-        break;
-      case 2:
-        if (blink == 0) lcd.createChar(0, Battery_2);
-        else lcd.createChar(0, Battery_3);
-        break;
-      case 3:
-        if (blink == 0) lcd.createChar(0, Battery_3);
-        else lcd.createChar(0, Battery_4);
-        break;
-      case 4:
-        if (blink == 0) lcd.createChar(0, Battery_4);
-        else lcd.createChar(0, Battery_5);
-        break;
-      case 5:
-        if (blink == 0) lcd.createChar(0, Battery_5);
-        else lcd.createChar(0, Battery_1);
-        break;
-    }
+    if (LCD_batteryBlinkState == 0) LCD_batteryChargePostBlink = LCD_batteryCharge;  // Bei 0 von Variable LCD_batteryBlinkState normalen Batteriestand (ohne Blink an) anzeigen
+    else LCD_batteryChargePostBlink = LCD_batteryCharge + 1;                         // Bei 1 von Variable LCD_batteryBlinkState Batteriestand (mit Blink an) anzeigen
   }
-  lcd.setCursor(0, 0);
-  lcd.write(0);
+  switch (LCD_batteryChargePostBlink) {  //Symbol je nach Batteriestand neu zuweisen
+    case 0:
+      lcd.createChar(0, LCD_batterySymbol0);
+      break;
+    case 1:
+      lcd.createChar(0, LCD_batterySymbol1);
+      break;
+    case 2:
+      lcd.createChar(0, LCD_batterySymbol2);
+      break;
+    case 3:
+      lcd.createChar(0, LCD_batterySymbol3);
+      break;
+    case 4:
+      lcd.createChar(0, LCD_batterySymbol4);
+      break;
+    case 5:
+      lcd.createChar(0, LCD_batterySymbol5);
+      break;
+  }
+  lcd.setCursor(0, 0);  //Cursor an gewünschte stelle für das Batterysymbol setzen
+  lcd.write(0);         //Batteriesysmbol schreiben
 }
 
 void setup() {
@@ -79,5 +56,8 @@ void setup() {
 }
 
 void loop() {
-  LCD_battery(1, 0);
+  LCD_battery();
+
+  LCD_batteryIsCharging = 1;
+  LCD_batteryCharge = 1;
 }
--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Jakob Schulz
 
 Kabelloses Numpad mit Zusatzfunktionen für die Steuerung eines Logimaten
 
-Zu installierende Biblioteken:
+Aktuelle Lokation: /home/jakobschulz/Dokumente/Arduino_Projekte/Numpad_Logimat/Code2/Numpad_Logimat
 
+Zu installierende Biblioteken:
 -LiquidCrystal_I2C von Martin Kubovcik, Frank de Brabander

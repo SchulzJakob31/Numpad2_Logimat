6242abb68123d3731d14eceaf2703c5a29a9d898
Jakob Schulz <jakobschulz83@gmail.com>
2026-08-01 14:23:19 +0200

    enum für einzelne PISO Kanäle hinzugefügt

--- Numpad_Logimat.ino
+++ Numpad_Logimat.ino
@@ -3,8 +3,8 @@
 #include <BleCombo.h>
 
 //========== NumPad allgemein ==========
-bool Shift_state = 0;  // Zustand von Shift (an oder aus)
-const uint8_t Shift_button = 7; //PISO_Kanal an der Shift angeschlossen ist
+bool Shift_state = 0;            // Zustand von Shift (an oder aus)
+const uint8_t Shift_button = 7;  //PISO_Kanal an der Shift angeschlossen ist
 
 //========== PISO ==========
 const uint8_t PISO_pinData = 15;  // Pin vom PISO von dem die Daten Seriell empfangen werden
@@ -14,6 +14,41 @@ const uint8_t PISO_pinClock = 4;  // Pin zum PISO mit dem die Daten weiter getak
 const uint8_t PISO_anzahl = 4;                       // Anzahl der Schieberegister
 const uint8_t PISO_anzahlKanaele = PISO_anzahl * 8;  // Anzahl der Eingangskanäle aller Schieberegister
 bool PISO_data[PISO_anzahlKanaele];                  // Rohe daten die vom Schieberegister empfangen wurden
+
+enum PISO_anzahlKanaele {
+  Num0,
+  Num1,
+  Num2,
+  Num3,
+  Num4,
+  Num5,
+  Num6,
+  Num7,
+  Num8,
+  Num9,
+  Num10,
+  Num11,
+  Num12,
+  Num13,
+  Num14,
+  Num15,
+  Num16,
+  Num17,
+  Num18,
+  Num19,
+  Num20,
+  Num21,
+  Num22,
+  Num23,
+  Num24,
+  Num25,
+  Num26,
+  Num27,
+  Num28,
+  Num29,
+  Num30,
+  Num31
+};
 
 
 void PISO_read() {
@@ -89,10 +124,12 @@ void LCD_battery() {
 void setup() {
   //Allgemein
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
+  pinMode(2, OUTPUT);  // Statusanzeige für Shift
+  
   // LCD
   lcd.init();       // LCD initialisieren
   lcd.backlight();  // LCD Hintergrundbeleuchtung anschalten
+
   // BLE Keyboard Mouse
   Keyboard.begin();  // BLE Keyboard starten
   Mouse.begin();     // BLE Mouse starten
@@ -104,6 +141,7 @@ void setup() {
   digitalWrite(PISO_pinClock, LOW);  // ein Clockzyklus zum Initialisieren
   digitalWrite(PISO_pinClock, HIGH);
   digitalWrite(PISO_pinClock, LOW);
+
   // Debouncing konfigurieren
   for (uint8_t a = 0; a < PISO_anzahlKanaele; a++) {
     PISO_debouncedState[a] = PISO_data[a];
@@ -112,7 +150,7 @@ void setup() {
     PISO_pressed[a] = 0;
   }
 
-  while (!Keyboard.isConnected()) {
+  while (!Keyboard.isConnected()) {  // Erst in den Loop übergehen wenn Bluetooth verbunden ist
   }
 }
 
@@ -128,11 +166,43 @@ void loop() {
   if (PISO_pressed[Shift_button]) Shift_state = !Shift_state;   // Wenn Shift gedrückt wird Shift umschalten
   digitalWrite(2, Shift_state);                                 // Zustand von Schift mit LED anzeigen
 
-  if (PISO_pressed[0]) Keyboard.println("STAN");    // STAN schreiben dann Enter
-  if (PISO_pressed[1]) Keyboard.print("STAN");      // STAN schreiben ohne Enter danach
-  if (PISO_pressed[2]) Keyboard.write(KEY_RETURN);  // Enter drückent
-  if (PISO_pressed[3]) Mouse.move(10, 10);
-  if (PISO_pressed[4]) Mouse.move(-10, -10);
+
+  if(!Shift_state) {
+  if (PISO_pressed[Num0]) Keyboard.println("STAN");    // STAN schreiben dann Enter
+  if (PISO_pressed[Num1]) Keyboard.print("STAN");      // STAN schreiben ohne Enter danach
+  if (PISO_pressed[Num2]) Keyboard.write(KEY_RETURN);  // Enter drückent
+  if (PISO_pressed[Num3]) Mouse.move(10, 10);
+  if (PISO_pressed[Num4]) Mouse.move(-10, -10);
+  if (PISO_pressed[Num5]) Keyboard.print("STAN0");
+  if (PISO_pressed[Num6]) Keyboard.print("STAN00");
+  //if (PISO_pressed[Num7])  // nicht verwendbar da Shift Taste
+  /*
+  if (PISO_pressed[Num8])
+  if (PISO_pressed[Num9])
+  if (PISO_pressed[Num10])
+  if (PISO_pressed[Num11])
+  if (PISO_pressed[Num12])
+  if (PISO_pressed[Num13])
+  if (PISO_pressed[Num14])
+  if (PISO_pressed[Num15])
+  if (PISO_pressed[Num16])
+  if (PISO_pressed[Num17])
+  if (PISO_pressed[Num18])
+  if (PISO_pressed[Num19])
+  if (PISO_pressed[Num20])
+  if (PISO_pressed[Num21])
+  if (PISO_pressed[Num22])
+  if (PISO_pressed[Num23])
+  if (PISO_pressed[Num24])
+  if (PISO_pressed[Num25])
+  if (PISO_pressed[Num26])
+  if (PISO_pressed[Num27])
+  if (PISO_pressed[Num28])
+  if (PISO_pressed[Num29])
+  if (PISO_pressed[Num30])
+  if (PISO_pressed[Num31])
+  */
+  }
 }
 
 /*
